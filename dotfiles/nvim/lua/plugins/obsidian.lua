10fc20a9a342b1756a8b0d377ded0438dc98a7b4
amatgil <amatgilvinyes@gmail.com>
2024-04-18 01:29:26 +0200

    Switching to awesome fully

--- dotfiles/nvim/lua/plugins/obsidian.lua
+++
@@ -1,7 +0,0 @@
--- REQUIRS NIGHTLY, sadge
-require("obsidian").setup({
-  dir = "/media/HDD/MainObsidianVault/",
-  completion = {
-    nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
-  }
-})
--- dotfiles/nvim/lua/plugins/parinfer.lua
+++
@@ -1,9 +0,0 @@
--- Aqui, totes les coses de liso i de vlime
-
--- Parinfer
-vim.g.parinfer_filetypes = {'lisp'}
-vim.g.parinfer_mode = 'paren'
-vim.g.lisp_rainbow = 1
-
--- Vlime
--- vim.g.vlime_compiler_policy = ["DEBUG" = 3] -- I'm not sure how lua works
