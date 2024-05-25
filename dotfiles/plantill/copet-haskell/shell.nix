fe47065d06cc920038728c89fb72b91b06625a14
amatgil <amatgilvinyes@gmail.com>
2024-05-25 13:02:29 +0200

    Added haskell support

---
+++ dotfiles/plantill/copet-haskell/shell.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    haskell-language-server
+    ghc
+    ghcid # Bacon for haskell
+    cabal-install
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
