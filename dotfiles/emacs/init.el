44f0780245eeafb7269b829ce27222a1cf6e8f13
amatgil <amatgilvinyes@gmail.com>
2024-09-06 05:42:54 +0200

    Fixed emacs updated options

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -57,7 +57,7 @@
       modus-themes-tabs-accented t
       modus-themes-paren-match '(bold intense)
       modus-themes-prompts '(bold intense)
-      modus-themes-completions 'opinionated
+      modus-themes-completions '((matches . (extrabold underline)) (selection . (semibold italic)))
       modus-themes-org-blocks 'tinted-background
       modus-themes-scale-headings t
       modus-themes-region '(bg-only)
