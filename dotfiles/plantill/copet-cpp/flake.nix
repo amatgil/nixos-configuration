ee5c50a79cf5ffc5c78fc67e67f577a3dcc5a1c8
amatgil <amatgilvinyes@gmail.com>
2024-11-10 18:32:06 +0100

    Add cpp plantill

---
+++ dotfiles/plantill/copet-cpp/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Default C++ flake (casenc)";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = (import nixpkgs { system = "x86_64-linux"; });
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+      });
+    };
+}
---
+++ dotfiles/plantill/copet-cpp/shell.nix
@@ -0,0 +1,20 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    gnumake
+    indent
+    man-pages man-pages-posix
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
