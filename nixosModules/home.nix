e32ce651a1c1e83e3c8001d6d8fc7d1f291cb2b1
amatgil <amatgilvinyes@gmail.com>
2026-02-13 12:18:35 +0100

    Tal

--- nixosModules/home.nix
+++ nixosModules/home.nix
@@ -162,6 +162,10 @@
     zed-editor
 
     jq exiftool
+
+    audacity
+
+    nixd
   ];
 
   programs = {
@@ -195,7 +199,6 @@
         glg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
 
         img="devour sxiv"; pdf="devour zathura";
-        cal="cal -m";
 
         v="nvim"; top="btm"; sys="systemctl";
         ifconfig="ip --brief --color address";
