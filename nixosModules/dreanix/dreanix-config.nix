d81bb1bc189e2590893131a14f62ae27f53f7a2d
amatgil <amatgilvinyes@gmail.com>
2026-05-07 13:54:01 +0200

    zram, tweak screenshots script

--- nixosModules/dreanix/dreanix-config.nix
+++ nixosModules/dreanix/dreanix-config.nix
@@ -18,6 +18,19 @@
     supportedFilesystems = [ "ntfs" ];
   };
 
+  # zram stuff
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 100;
+  };
+
   services.openssh = {
     enable = true;
     ports = [ 22 ];
